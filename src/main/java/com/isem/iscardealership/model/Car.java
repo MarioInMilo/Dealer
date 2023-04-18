76c240644388638c64f4369b3e746575b5f1ffba
ivan <ivan-semashko@outlook.com>
2023-04-19 02:49:22 +0300

    Adding fields to the Car class: ID, BRAND HORSEPOWER, PRICE, ENGINETYPE

--- src/main/java/com/isem/iscardealership/model/Car.java
+++ src/main/java/com/isem/iscardealership/model/Car.java
@@ -1,4 +1,11 @@
 package com.isem.iscardealership.model;
 
 public class Car {
+    private long id;
+    private String brand;
+    private int horsePower;
+    private int price;
+    private EngineType engineType;
+
+
 }
