a91d5f56eee9c0a647d02bec4fc31a7b985848d4
ivan <ivan-semashko@outlook.com>
2023-04-19 02:32:32 +0300

    Init project

---
+++ src/main/java/com/isem/iscardealership/IsCarDealerShipApplication.java
@@ -0,0 +1,13 @@
+package com.isem.iscardealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsCarDealerShipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsCarDealerShipApplication.class, args);
+    }
+
+}
