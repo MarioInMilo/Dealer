6753df912e7e1527a135410d8a53eeff3eddb030
ivan <ivan-semashko@outlook.com>
2023-04-19 02:53:35 +0300

    Adding fields to the User class: ID, USERNAME, LOGIN, PASSWORD, ROLE, EMAIL, BAN

--- src/main/java/com/isem/iscardealership/model/User.java
+++ src/main/java/com/isem/iscardealership/model/User.java
@@ -1,4 +1,11 @@
 package com.isem.iscardealership.model;
 
 public class User {
+    private long id;
+    private String username;
+    private String login;
+    private String password;
+    private Role role;
+    private String email;
+    private boolean ban;
 }
