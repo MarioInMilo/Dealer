3d8976444cf1a9b44bace79036bd304797a4c1a0
ivan <ivan-semashko@outlook.com>
2023-04-19 17:32:24 +0300

    Adding annotations for Car

--- src/main/java/com/isem/iscardealership/model/Car.java
+++ src/main/java/com/isem/iscardealership/model/Car.java
@@ -1,10 +1,29 @@
 package com.isem.iscardealership.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cars")
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    private String brand;
+    @Column(name = "brand")
+    @Enumerated(EnumType.STRING)
+    private Brand brand;
+    @Column(name = "horsepower")
     private int horsePower;
+    @Column(name = "price")
     private int price;
+    @Column(name = "enginetype")
+    @Enumerated(EnumType.STRING)
     private EngineType engineType;
 
 
