dd0083b5d9a1dd9a0c0fe246eee2295fc7006381
ivan <ivan-semashko@outlook.com>
2023-04-19 17:32:14 +0300

    Adding annotations for User

--- src/main/java/com/isem/iscardealership/model/User.java
+++ src/main/java/com/isem/iscardealership/model/User.java
@@ -1,11 +1,31 @@
 package com.isem.iscardealership.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @Column(name = "username")
     private String username;
+    @Column(name = "login")
     private String login;
+    @Column(name = "password")
     private String password;
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
     private Role role;
+    @Column(name = "email", unique = true)
     private String email;
+    @Column(name = "ban")
     private boolean ban;
 }
